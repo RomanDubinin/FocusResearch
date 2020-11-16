cbd5265f9b2f6c625553b70b1b34a07c2f4716b2
RomanDubinin <romandubinin94@gmail.com>
2020-11-16 20:54:10 +0100

    client

---
+++ Download/FocusClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FocusResearch
+{
+    public class FocusClient
+    {
+        private string key { get; }
+        private HttpClient httpClient { get; }
+
+        public FocusClient(Uri baseUri, string key)
+        {
+            this.key = key;
+            httpClient = new HttpClient
+            {
+                BaseAddress = baseUri,
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+        }
+
+        public async Task<string> ReadBookkeepingReportAsync(string inn, string ogrn)
+        {
+            return await httpClient.GetStringAsync($"buh?inn={inn}&ogrn={ogrn}&key={key}").ConfigureAwait(false);
+        }
+    }
+}
---
+++ Download/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using FocusResearch;
+
+namespace Download
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var focusClient = new FocusClient(new Uri("https://focus-api.kontur.ru/api3/"), args[0]);
+            var bo = await focusClient.ReadBookkeepingReportAsync("6164079184", "1026103281500").ConfigureAwait(false);
+            Console.WriteLine(bo);
+        }
+    }
+}
