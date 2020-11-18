56c0b1e8ef870b842f7d499d23e359738d66330a
RomanDubinin <romandubinin94@gmail.com>
2020-11-18 18:33:16 +0100

    working prototype

--- Download/Program.cs
+++ Download/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using FocusResearch;
 
@@ -8,9 +9,31 @@ namespace Download
     {
         static async Task Main(string[] args)
         {
+            var file = @"Files\Фокус-Контрагенты-2020-11-18.xlsx";
+            var directory = Path.GetDirectoryName(file);
+            var extendedRawDataFileName = Path.Combine(directory, Path.GetFileNameWithoutExtension(file) + "_extended_raw.txt");
+            if (File.Exists(Path.Combine(directory, extendedRawDataFileName)))
+                File.Delete(Path.Combine(directory, extendedRawDataFileName));
+            var exelReader = new ExelReader();
+            var identities = exelReader.GetOrganizationIdentifiers(file);
+            Console.WriteLine(identities.Count + " organizations to process.");
+
             var focusClient = new FocusClient(new Uri("https://focus-api.kontur.ru/api3/"), args[0]);
-            var bo = await focusClient.ReadBookkeepingReportAsync("6164079184", "1026103281500").ConfigureAwait(false);
-            Console.WriteLine(bo);
+
+            var i = 0;
+            foreach (var identity in identities)
+            {
+                var bo = await focusClient.ReadBookkeepingReportAsync(identity.inn, identity.ogrn).ConfigureAwait(false);
+                await File.AppendAllTextAsync(extendedRawDataFileName, MakeRawRow(identity, bo)).ConfigureAwait(false);
+                Console.WriteLine(bo.Length);
+                Console.WriteLine(i++);
+                Console.WriteLine("----");
+            }
+        }
+
+        private static string MakeRawRow((string inn, string ogrn) identity, string content)
+        {
+            return $"{identity.inn}, {identity.ogrn}, {content}\n";
         }
     }
 }
